de7391bd97d51628540d82041b5a52bca8b280fa
Cody Baermann <cbaermann@comcast.net>
2019-12-07 01:03:24 -0800

    build deck function that creates a new deck using two slices of cardSuit and cardValue

--- cards/deck.go
+++ cards/deck.go
@@ -6,6 +6,20 @@ import "fmt"
 //which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+	//use underscore when not using i or j or any var indicator. tells go essentialy that you know you're not using them.
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
 //d deck is a reciever. any variable of type deck now gets access to print method.
 func (d deck) print() {
 	//index, card = current card iterating over, take slice of 'cards' and loop over it
