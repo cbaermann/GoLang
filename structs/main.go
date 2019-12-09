cb6da6b4235e1852460770c184cfafb235b255e6
Cody Baermann <cbaermann@comcast.net>
2019-12-09 06:41:24 -0800

    added fN and lN in one struct, contact info in another

--- structs/main.go
+++ structs/main.go
@@ -2,14 +2,34 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
-	alex := person{firstName: "Alex", lastName: "Volkanovski"}
-	// max := person{firstName: "Max", lastName: "Holloway"}
+	alex := person{
+		firstName: "Alex",
+		lastName:  "Volkanovski",
+		contactInfo: contactInfo{
+			email:   "alex@tigermuaythai.com",
+			zipCode: 83000,
+		},
+	}
+	// %+v shows the key value within the scruct
+	alex.updateName("Alexander")
+	alex.print()
+}
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
 
-	fmt.Println(alex)
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
