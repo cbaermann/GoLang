4b1185f81c01d4edda3953529b2ae7f43a657e1d
Cody Baermann <cbaermann@comcast.net>
2019-12-09 14:49:05 -0800

    added pointers to update values

--- structs/main.go
+++ structs/main.go
@@ -22,12 +22,22 @@ func main() {
 			zipCode: 83000,
 		},
 	}
-	// %+v shows the key value within the scruct
-	alex.updateName("Alexander")
+	// %+v shows the key within the scruct
+	// & is an operator. gives access to where alex value is.
+	alexPointer := &alex
+	alexPointer.updateName("Alexander")
 	alex.print()
 }
-func (p person) updateName(newFirstName string) {
-	p.firstName = newFirstName
+
+//* operator followed by memory address, or pointer. pointertoperson is memory address that alex lives at
+//* in front of type is different than * in front of pointer.
+//*person in a place where type is supposed to be, it is a description of a type. here, looking for a pointer to a person.
+//*pointertoperson. operator that takes pointer and assigns to a value. Here, references first name property then updates it to newFirstName
+
+//turn address into value with *address
+//turn value into address with &value
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
 }
 
 func (p person) print() {
