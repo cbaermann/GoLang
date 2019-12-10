83eb0b8af684e0f8b5d1ec9c463e9450ff042fdc
Cody Baermann <cbaermann@comcast.net>
2019-12-10 14:35:31 -0800

    dont know if last commit worked

--- map/main.go
+++ map/main.go
@@ -22,3 +22,11 @@ func printMap(c map[string]string) {
 		fmt.Println("Hex code for", color, "is", hex)
 	}
 }
+
+//map vs struct
+//map- all keys must be of same type, all values must be of same time
+//map-all different keys are index'd, can iterate over key value pairs
+//can not iterate over all key value pairs in struct
+//map is a reference type, struct is a value type.
+//general guideline, use map when using collection of closley related properties.
+//map-do not need to know of a list of keys or field names at compiled time. can add and remove over time when you want.
