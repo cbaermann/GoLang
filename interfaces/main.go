b062e1414f78b3828de8d6a59b6a590ed5f4146e
Cody Baermann <cbaermann@comcast.net>
2019-12-12 07:24:49 -0800

    added interface of type bot to program

--- interfaces/main.go
+++ interfaces/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+//calls all getGreeting() of type string
+//calls print greeting because it is of type bot
+type bot interface {
+	getGreeting() string
+}
+
 type englishBot struct{}
 type spanishBot struct{}
 
@@ -10,16 +16,12 @@ func main() {
 	sb := spanishBot{}
 
 	printGreeting(eb)
-	// printGreeting(sb)
+	printGreeting(sb)
 }
 
-func printGreeting(eb englishBot) {
-	fmt.Println(eb.getGreeting)
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
-
-// func printGreeting(sb spanishBot) {
-// 	fmt.Println(sb.getGreeting)
-// }
 
 func (englishBot) getGreeting() string {
 	//very custom logic for generating an english greeting
