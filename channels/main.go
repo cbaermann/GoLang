9d6ab083daedc28d9b739f0e4f8416a82d530ab5
Cody Baermann <cbaermann@comcast.net>
2019-12-17 21:08:53 -0800

    introducing channels into calls

--- channels/main.go
+++ channels/main.go
@@ -13,18 +13,26 @@ func main() {
 		"http://golang.org",
 		"http://amazon.com",
 	}
+
+	c := make(chan string)
+
 	for _, link := range links {
-		go checkLink(link)
+		//go main routine makes child routines
+		//main routine controlls when it exits/quits
+		go checkLink(link, c)
 	}
+	fmt.Println(<-c)
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "Might be down"
 		return
 	}
 
-	fmt.Println(link, "is working!s")
+	fmt.Println(link, "is working!")
+	c <- "Yup it's up"
 
 }
